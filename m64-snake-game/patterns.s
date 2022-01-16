0da47d840180023257c1b1ad37b1128aff236920
E4tHam <ethan@sifferman.dev>
2022-01-16 13:38:13 -0800

    added black, white, and fruit patterns

---
+++ m64-snake-game/patterns.s
@@ -0,0 +1,14 @@
+
+.CODE
+
+.export _black_pattern
+_black_pattern:
+.incbin "patterns/black.pat"
+
+.export _white_pattern
+_white_pattern:
+.incbin "patterns/white.pat"
+
+.export _fruit_pattern
+_fruit_pattern:
+.incbin "patterns/fruit.pat"
